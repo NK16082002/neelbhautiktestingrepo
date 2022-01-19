8b39c9334915b381e34d7064a64ae76720f2e20d
Bhautik Ramani <bmramani2503@gmail.com>
2022-01-19 15:29:09 +0530

    sum and mul cpp file commit

---
+++ mul.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    int a=10,b=20;
+    cout<<a*b;
+    return 0;
+}
